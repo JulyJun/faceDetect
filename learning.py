76422f1f30c18cb90da1e8102174caa633c9c5cf
JulyJun <bag3401@gmail.com>
2024-03-11 17:09:05 +0900

    done

--- learning.py
+++ learning.py
@@ -1,9 +1,10 @@
 import cv2
 from ultralytics import YOLO
+from ultralytics.utils.plotting import Annotator  # ultralytics.yolo.utils.plotting is deprecated
 
-def apply_mosaic(image, results, scale=0.1):
-    for r in results.xyxy[0].cpu().numpy():
-        x, y, w, h = map(int, r[:4])
+def apply_mosaic(image, boxes, scale=0.1):
+    for box in boxes:
+        x, y, w, h = map(int, box)
         roi = image[y:y+h, x:x+w]
         roi = cv2.resize(roi, (0, 0), fx=scale, fy=scale)
         roi = cv2.resize(roi, (w, h), interpolation=cv2.INTER_NEAREST)
@@ -25,11 +26,18 @@ while cap.isOpened():
 
     if success:
         # Run YOLOv8 inference on the frame
-        results = model(frame, conf=0.4)
+        results = model.predict(frame, conf=0.4, show=False, verbose=False)
 
         # Apply mosaic effect over detected objects
-        frame = apply_mosaic(frame, results)
-
+        for r in results:
+            annotator = Annotator(frame)
+            boxes = r.boxes
+            for box in boxes:
+                b = box.xyxy[0]
+                c = box.cls
+                annotator.box_label(b, color=(0, 0, 255))
+        
+        frame = annotator.result()
         # Display the annotated frame
         cv2.imshow("YOLOv8 Inference with Mosaic", frame)
 
--- yolotest.py
+++ yolotest.py
@@ -2,8 +2,8 @@ from ultralytics import YOLO
 #from ultralytics.yolo.v8.detect.predict import DetectionPredictor
 import cv2
 
-model = YOLO("best.pt")
-src = "0"
+model = YOLO("yolov8x.pt")
+src = "https://www.youtube.com/watch?v=M8QoC6iMO40"
 #result = model.predict(source=src,conf=0.5, show=True)
 #print(result.boxes)
 results = model(source=src,conf=0.5, show=True)
